556e486772041df1ca04c16ba6d23989d182bb81
Amnecia <andreasrafaeltobing@gmail.com>
2024-06-01 10:58:41 +0700

    Sixth Changes

--- app.py
+++ app.py
@@ -12,5 +12,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route('/about', methods=['GET'])
+def tambah_produk():
+    return render_template('tambah_produk.html')
+
+@app.route('/about', methods=['GET'])
+def edit_produk():
+    return render_template('edit_produk.html')
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
