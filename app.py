30744b4926a1540121ce396cec825d95f03cfbb0
Amnecia <andreasrafaeltobing@gmail.com>
2024-06-04 19:14:34 +0700

    Eight Changes

--- app.py
+++ app.py
@@ -77,6 +77,10 @@ def SetStatus():
 def login():
     return render_template('login.html')
 
+@app.route('/etalase', methods=['GET'])
+def etalase():
+    return render_template('etalase.html')
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
